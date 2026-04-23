2894966cf3509fbbb1c28cd8e99ef427523745d4
Елисеенко Алеся Алексеевна <eaa333@list.ru>
2026-04-23 10:15:31 +0300

    Изменила работу ввода номера телефона. Теперь прога не дает вводить спец.символы и буквы, а только цифры.

--- ContactManager.cs
+++ ContactManager.cs
@@ -2,55 +2,59 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using WorkWithContacts;
 
-public class ContactManager
+namespace WorkWithContacts
 {
-    public List<Contact> Contacts { get; private set; }
-    public ContactManager()
+    public class ContactManager
     {
-        Contacts = new List<Contact>();
-        LoadContacts();
-    }
-    public void AddContact(Contact contact)
-    {
-        if (contact == null)
-        {
-            throw new ArgumentNullException(nameof(contact));
-        }
-        Contacts.Add(contact);
-        SaveContacts();
-    }
+        public List<Contact> Contacts { get; private set; }
 
-    public void RemoveContact(Contact contact)
-    {
-        if (contact == null)
+        public ContactManager()
         {
-            throw new ArgumentNullException(nameof(contact));
+            Contacts = new List<Contact>();
+            LoadContacts();
         }
-        Contacts.Remove(contact);
-        SaveContacts();
-    }
-    public List<Contact> SearchContacts(string query)
-    {
-        return Contacts.Where(c => c.Name.Contains(query) ||
-        c.PhoneNumber.Contains(query)).ToList();
-    }
-    private void SaveContacts()
-    {
-        File.WriteAllLines("contacts.txt", Contacts.Select(c => $"{c.Name}|{c.PhoneNumber}"));
-    }
-    private void LoadContacts()
-    {
-        if (File.Exists("contacts.txt"))
+
+        public void AddContact(Contact contact)
         {
-            var lines = File.ReadAllLines("contacts.txt");
-            foreach (var line in lines)
+            if (contact == null)
+                throw new ArgumentNullException(nameof(contact));
+
+            Contacts.Add(contact);
+            SaveContacts();
+        }
+
+        public void RemoveContact(Contact contact)
+        {
+            if (contact == null)
+                throw new ArgumentNullException(nameof(contact));
+
+            Contacts.Remove(contact);
+            SaveContacts();
+        }
+
+        public List<Contact> SearchContacts(string query)
+        {
+            return Contacts.Where(c => c.Name.Contains(query) || c.PhoneNumber.Contains(query)).ToList();
+        }
+
+        private void SaveContacts()
+        {
+            File.WriteAllLines("contacts.txt", Contacts.Select(c => $"{c.Name}|{c.PhoneNumber}"));
+        }
+
+        private void LoadContacts()
+        {
+            if (File.Exists("contacts.txt"))
             {
-                var parts = line.Split('|');
-                if (parts.Length == 2)
+                var lines = File.ReadAllLines("contacts.txt");
+                foreach (var line in lines)
                 {
-                    Contacts.Add(new Contact(parts[0], parts[1]));
+                    var parts = line.Split('|');
+                    if (parts.Length == 2)
+                    {
+                        Contacts.Add(new Contact(parts[0], parts[1]));
+                    }
                 }
             }
         }
